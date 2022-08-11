9dbb84eb84b25b3a7d05d7b3dc8f2991219cce7d
Vaibhav-afk <vbhvka@gmail.com>
2022-08-11 17:43:03 +0530

    added google map along with rental list

--- src/components/RentalsMap.js
+++ src/components/RentalsMap.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Marker, GoogleApiWrapper, Map } from "google-maps-react";
 
-function RentalsMap() {
+//note that google inside the below prop is coming from GoogleApiWrapper.
+function RentalsMap({ locations, google, setHighLight }) {
+  const [center, setCenter] = useState();
+
+  useEffect(() => {
+    //Object.Keys takes object as parameter and return array.
+    let arr = Object.keys(locations);
+    let getLat = (key) => locations[key]["lat"];
+    let avgLat = arr.reduce((a, c) => a + Number(getLat(c), 0)) / arr.length;
+
+    let getLng = (key) => locations[key]["lng"];
+    let avgLng = arr.reduce((a, c) => a + Number(getLng(c)), 0) / arr.length;
+
+    setCenter({ lat: avgLat, lng: avgLng });
+  }, [locations]);
 
   return (
     <>
-      <div>Map</div>
+      {center && (
+        <Map
+          google={google}
+          containerStyle={{
+            width: "50vw",
+            height: "calc(100vh - 135px)",
+          }}
+          center={center}
+          initialCenter={locations[0]}
+          zoom={13}
+          disableDefaultUI={true}
+        >
+          {locations.map((coords, i) => (
+            <Marker position={coords} onClick={() => setHighLight(i)} />
+          ))}
+        </Map>
+      )}
     </>
   );
 }
 
-export default RentalsMap;
+export default GoogleApiWrapper({
+  apiKey: "",
+})(RentalsMap);
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
 
+import bg from "../images/frontpagebg.png";
+import logo from "../images/airbnb.png";
+import { ConnectButton } from "@web3uikit/web3";
+import { Select, DatePicker, Input } from "@web3uikit/core";
+import { Search } from "@web3uikit/icons";
+
 const Home = () => {
+  const [checkIn, setCheckIn] = useState(new Date());
+  const [checkOut, setCheckOut] = useState(new Date());
+  const [destination, setDestination] = useState("New York");
+  const [guests, setGuests] = useState(2);
 
   return (
     <>
-      <Link to="/rentals"> Rentals </Link>
+      <div className="container" style={{ backgroundImage: `url(${bg})` }}>
+        <div className="containerGradinet"></div>
+      </div>
+
+      <div className="topBanner">
+        <div>
+          <img className="logo" src={logo} alt="logo"></img>
+        </div>
+
+        <div className="tabs">
+          <div className="selected">Places To Stay</div>
+          <div>Experiences</div>
+          <div>Online Experiences</div>
+        </div>
+
+        <div className="lrContainers">
+          <ConnectButton />
+        </div>
+      </div>
+
+      <div className="tabContent">
+        <div className="searchFields">
+          <div className="inputs">
+            Location
+            <Select
+              defaultOptionIndex={0}
+              onChange={(data) => setDestination(data.label)}
+              options={[
+                { id: "ny", label: "New York" },
+                { id: "lon", label: "London" },
+                { id: "db", label: "Dubai" },
+                { id: "la", label: "Los Angeles" },
+              ]}
+            />
+          </div>
+          <div className="vl" />
+          <div className="inputs">
+            Check In
+            <DatePicker
+              id="CheckIn"
+              onChange={(event) => setCheckIn(event.date)}
+            />
+          </div>
+          <div className="vl" />
+          <div className="inputs">
+            Check Out
+            <DatePicker
+              id="CheckOut"
+              onChange={(event) => setCheckOut(event.date)}
+            />
+          </div>
+          <div className="vl" />
+          <div className="inputs">
+            Guests
+            <Input
+              value={2}
+              name="AddGuests"
+              type="number"
+              onChange={(event) => setGuests(Number(event.target.value))}
+            />
+          </div>
+
+          <Link
+            to={"/rentals"}
+            state={{ destination, checkIn, checkOut, guests }}
+          >
+            <div className="searchButton">
+              <Search fill="#fff" size={24}  />
+            </div>
+          </Link>
+        </div>
+      </div>
     </>
   );
 };
